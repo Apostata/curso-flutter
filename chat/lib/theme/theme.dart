326a66ad2a352d117485ca05b89210575732d36a
Rene <rene.souza@fcamara.com.br>
2021-11-25 11:28:30 -0300

    iniciando a aplicação mas deixando a integração com o firebase para o final

---
+++ chat/lib/theme/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+final theme = ThemeData(
+  primarySwatch: Colors.blue,
+  textTheme: const TextTheme(
+    bodyText2: TextStyle(
+      color: Colors.white
+    ),
+    headline6: TextStyle(
+      color: Colors.white
+    )
+  )
+);
